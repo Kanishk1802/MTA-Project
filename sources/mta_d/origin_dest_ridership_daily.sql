8e6e751e6995709c5d20b72a724c686e6a9936cf
Kanishk Dutta <duttakanishk.kd@gmail.com>
2024-10-27 16:27:55 -0400

    new linking format

--- sources/mta_d/origin_dest_ridership_daily.sql
+++ sources/mta_d/origin_dest_ridership_daily.sql
@@ -1,1 +1,10 @@
-SELECT * FROM or_des_rd
+SELECT 
+    *,
+    FIRST_VALUE(row_num) OVER (PARTITION BY Origin_Station_Complex_Name) AS link_friendly_id
+FROM (
+    SELECT 
+        *,
+        ROW_NUMBER() OVER (PARTITION BY Origin_Station_Complex_Name ORDER BY Origin_Station_Complex_Name) AS row_num
+    FROM or_des_rd
+) AS subquery
+
